8d4115db55dbc8073b724e40152b354b09c1d7e7
jeff <jeff.degraef@dimensiondata.com>
2018-05-01 00:57:00 +0200

    Linting

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,22 +1,33 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+
+//ensures linting errors for console are not shown
+/* eslint-disable no-console */
+
+//import webpack
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
+//Express server
 const port = 3000;
 const app = express();
+
+//Webpack compiler
 const compiler = webpack(config);
 
+//attached webpack compiler to express server
 app.use(require('webpack-dev-middleware')(compiler,{
     noInfo:true,
     publicPath:config.output.publicPath
 }));
 
+//main route
 app.get('/',function (req,res) {
     res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+//launch webserver on port 3000
 app.listen(port,function (err) {
     if(err){
         console.log(err);
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,5 +1,7 @@
 import chalk from 'chalk';
 //var chalk = require('chalk');
+//ensures linting errors for console are not shown
+/* eslint-disable no-console */
 
 console.log(chalk.green("starting app in dev mode..."));
 
